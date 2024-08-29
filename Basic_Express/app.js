a8510e98c5eec05775bcec3c945075bfaae5a68e
AASTHA2020 <modiaastha0@gmail.com>
2024-08-30 01:17:45 +0530

    first express code

---
+++ Basic_Express/app.js
@@ -0,0 +1,18 @@
+import express from 'express'; // Use import instead of require
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
