727a4cd9297df9c19777371980d2c15055e1fa37
AASTHA2020 <modiaastha0@gmail.com>
2024-09-05 00:43:00 +0530

    bcrypt basics

--- JWT/app.js
+++ JWT/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const userModel = require('./models/user');
-
+const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -12,21 +11,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/create', async (req, res) => {
+app.post('/create', (req, res) => {
     let {username, email, password, age} = req.body;
 
-    let createdUser = await userModel.create({
-        username: username,
-        email: email,
-        password: password,
-        age: age
-    })
-    res.send(createdUser);
+   bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        let createdUser = await userModel.create({
+            username: username,
+            email: email,
+            password: hash,
+            age: age
+        });
+        res.send(createdUser);
+      });
+   });
 });
 
 app.listen(3000, () => {
